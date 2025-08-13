b82328ec4bb30b787d724d32eb477f25d9f43d9a
arunuxid <arz.pro.arz+github@gmail.com>
2025-08-13 18:45:57 +0530

    Jenna Buchholz Project

---
+++ Jenna Buchholz/scroll.js
@@ -0,0 +1,61 @@
+const pixelsTag = document.querySelector("div.pixels");
+const bodyTag = document.querySelector("body");
+const progressTag = document.querySelector("div.progress");
+const sections = document.querySelectorAll("section");
+const clientTag = document.querySelector("div.client");
+const pageTag = document.querySelector("div.page");
+const headerTag = document.querySelector("header");
+
+document.addEventListener("scroll", function(){
+
+    const pixels = window.pageYOffset;
+    pixelsTag.innerHTML = Math.round(pixels);
+
+    const pageHeight = bodyTag.getBoundingClientRect().height;
+    const totalScrollableDistance = pageHeight - window.innerHeight;
+
+    const percentage = (pixels / totalScrollableDistance);
+
+    progressTag.style.width = `${100 * percentage}%`;
+    
+});
+
+document.addEventListener("scroll", function(){
+
+    const pixels = window.pageYOffset;
+
+    sections.forEach( section => {
+        if(section.offsetTop - 100 < pixels){
+            clientTag.innerHTML = section.getAttribute("data-client")
+            pageTag.innerHTML = section.getAttribute("data-page");
+
+            if(section.hasAttribute("data-is-dark")){
+                headerTag.classList.add("white");
+                progressTag.classList.add("white");
+            } else {
+                headerTag.classList.remove("white");
+                progressTag.classList.remove("white");
+            }
+        }
+    })
+});
+
+
+document.addEventListener("scroll", function(){
+    const topViewport = window.pageYOffset;
+    const midViewport = topViewport + (window.innerHeight / 2);
+
+    sections.forEach(section => {
+        const topSection = section.offsetTop;
+        const midSection = topSection + (section.offsetHeight / 2);
+
+        const distanceToSection = midViewport - midSection;
+
+        const parralaxTag = section.querySelectorAll("[data-parallax]");
+
+        parralaxTag.forEach(tag => {
+            const speed = tag.getAttribute("data-parallax");
+            tag.style.transform = `translate(0, ${distanceToSection * speed}px)`;
+        });
+    });
+});
