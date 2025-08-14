7a5beb060a16e24ad0a3657e9d5361e8f90ea598
arunuxid <arz.pro.arz+github@gmail.com>
2025-08-14 20:51:11 +0530

    Added Project Brux

---
+++ Brux/animation.js
@@ -0,0 +1,38 @@
+const animatedTags = document.querySelectorAll("h2, h3 , p, section img, a.button");
+
+animatedTags.forEach(tag => {
+    tag.style.opacity = 0;
+});
+
+
+const fadeIn = function() {
+
+    let delay = 0.25;
+
+    animatedTags.forEach(tag => {
+        const tagTop = tag.getBoundingClientRect().top;
+        const tagBottom = tag.getBoundingClientRect().bottom;
+        
+        if( tagTop < window.innerHeight &&  tagBottom > 0){
+            tag.style.animation = `fadein 1s ${delay}s both`;
+            delay = delay + 0.15;
+        } else {
+            tag.style.opacity = 0;
+            tag.style.animation = "";
+        }
+    });
+    
+};
+
+// On load run fade in
+fadeIn();
+
+// on scroll run fade in
+document.addEventListener("scroll", function(){
+    fadeIn();
+});
+
+// on browser resize run fade in
+window.addEventListener("resize", function(){
+    fadeIn();
+});
